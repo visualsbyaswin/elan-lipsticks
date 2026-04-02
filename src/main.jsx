7dec4ca729831e141577e92940003d69bffca737
visualbyaswin <visuals.aswin@gmail.com>
2026-04-02 17:32:20 +0530

    Fix github pages router basename

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const DummyContent = ({ text }) => <p>{text}</p>;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
